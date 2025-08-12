5862a917ceec0674d186d634714241424766eb56
MNAMATELI ANDANI <admnamateli@gmail.com>
2025-08-12 19:26:00 +0200

    submit

--- src/App.jsx
+++ src/App.jsx
@@ -52,7 +52,7 @@ function App() {
           <div className="product-card" key={product.id}>
             <img src={product.image} alt={product.title} />
             <h3>{product.title}</h3>
-            <p>${product.price.toFixed(2)}</p>
+            <p>R{product.price.toFixed(2)}</p>
           </div>
         ))}
       </div>
